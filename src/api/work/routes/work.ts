ac17c957104ee587ef622cc4fd24c803d0047b67
Reihane Blosenhauer <reihaneb@liome.fr>
2024-03-16 15:14:28 +0100

    Use slug instead of id in work query

--- src/api/work/routes/work.ts
+++ src/api/work/routes/work.ts
@@ -1,7 +1,52 @@
+"use strict";
+
 /**
- * work router
+ * work router.
  */
 
-import { factories } from '@strapi/strapi';
+const { createCoreRouter } = require("@strapi/strapi").factories;
+const defaultRouter = createCoreRouter("api::work.work");
 
-export default factories.createCoreRouter('api::work.work');
+// function to add to or override default router methods
+const customRouter = (innerRouter, routeOveride = [], extraRoutes = []) => {
+  let routes;
+
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes;
+
+      const newRoutes = routes.map((route) => {
+        let found = false;
+
+        routeOveride.forEach((overide) => {
+          if (
+            route.handler === overide.handler &&
+            route.method === overide.method
+          ) {
+            found = overide;
+          }
+        });
+
+        return found || route;
+      });
+
+      return newRoutes.concat(extraRoutes);
+    },
+  };
+};
+
+// Overide the default router with the custom router to use slug.
+const myOverideRoutes = [
+  {
+    method: "GET",
+    path: "/works/:slug",
+    handler: "api::work.work.findOne",
+  },
+];
+
+const myExtraRoutes = [];
+
+module.exports = customRouter(defaultRouter, myOverideRoutes, myExtraRoutes);
