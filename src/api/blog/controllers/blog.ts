7a36a2f9e9a59e44c1609a1afabfa2bfb50dbdd2
Reihane Blosenhauer <reihaneb@liome.fr>
2024-05-09 16:17:16 +0200

    Implementation of blog articles

---
+++ src/api/blog/controllers/blog.ts
@@ -0,0 +1,26 @@
+// TODO: Fix the ts-nocheck when the issue is resolved
+// @ts-nocheck
+"use strict";
+
+/**
+ *  blog controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::blog.blog", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+
+    const blog = await strapi.entityService.findMany("api::blog.blog", query);
+
+    const sanitizedEntity = await this.sanitizeOutput(blog);
+
+    return this.transformResponse(sanitizedEntity[0]);
+  },
+}));
